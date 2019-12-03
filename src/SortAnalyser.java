03c0ad22450b5216f80aa031cc1c52c79b98eade
rohan-ubuntu <rst170000@utdallas.edu>
2019-12-03 01:27:26 -0600

    function to get random value

--- src/SortAnalyser.java
+++ src/SortAnalyser.java
@@ -1,5 +1,13 @@
+import java.util.Random;
 public class SortAnalyser {
     public static void main(String[] args) {
+
+    }
+
+    public int give_random_number(){
+        int random_num =10;
+
+
 
     }
 }
